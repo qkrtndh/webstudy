3c3394fab7d15a64ab45a705d0add915507d9071
qkrtndh <qkrtndh5@naver.com>
2021-02-06 12:40:39 +0900

    스크립트를 별도의 파일로 분할

---
+++ dnchange.js
@@ -0,0 +1,33 @@
+
+var body = {
+    setBackgroundColor: function (color) {
+        document.querySelector('body').style.backgroundColor = color;
+    },
+    setColor: function (color) {
+        document.querySelector('body').style.color = color;
+    }
+}
+var links = {
+    setColor: function (color) {
+        var array = document.querySelectorAll('a');
+        var i = 0;
+        for (i = 0; i < array.length; i++) {
+            array[i].style.color = color;
+        }
+    }
+}
+function changemode(self) {
+    var target = document.querySelector('body');
+    if (self.value == 'switch-N') {
+        body.setBackgroundColor('black');
+        body.setColor('white');
+        self.value = 'switch-D';
+        links.setColor('powderblue');
+    }
+    else {
+        body.setBackgroundColor('white');
+        body.setColor('black');
+        self.value = 'switch-N';
+        links.setColor('blue');
+    }
+}
